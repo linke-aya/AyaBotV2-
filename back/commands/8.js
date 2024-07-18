68a13ecfe372f30288da4112cecffc5b1068a8a2
linke-aya <ahmdaltwm995@gmail.com>
2024-07-18 07:50:15 +0100

    12

--- back/commands/8.js
+++ back/commands/8.js
@@ -1,47 +1,29 @@
 const axios = require("axios");
-
+const { getGroup, updateGroup } = require('../mongoose/thread');
+const log = require('../global/logger')
 module.exports = {
-    name: "سورة",
-    type: 'الدين',
+    name: "بادئة",
+    type: 'نظام',
+    otherName: ['البادئة', 'prefix'],
     creator: 'لنك',
     version: "1.0.0",
     usageCount: 0,
     run: async (api, event) => {
         const { threadID, messageID, body } = event;
-        const args = body.trim().split(" ");
-        const surahName = args.slice(1).join(" ");
-        const maxMessageLength = 2000; // الحد الأقصى لعدد الأحرف في رسالة فيسبوك
+        const args = body.split(' ').slice(1)
+        const prefixN = args[0]
+        const group = await getGroup(threadID)
+        if (!group) return
 
-        if (!surahName) {
-            return api.sendMessage("⚠️ |يرجى إرسال اسم السورة.", threadID, messageID);
-        }
-
-        try {
-            // استدعاء API للحصول على بيانات السورة
-            const response = await axios.get(`https://api.alquran.cloud/v1/surah/${encodeURIComponent(surahName)}/ar.asad`);
-            const surahData = response.data;
-
-            if (surahData.status !== "OK") {
-                return api.sendMessage("لم يتم العثور على السورة. يرجى التحقق من اسم السورة.", threadID, messageID);
-            }
-
-            let ayahsText = "";
-            for (const ayah of surahData.data.ayahs) {
-                ayahsText += `\n${ayah.numberInSurah}. ${ayah.text}`;
-            }
-
-            // تقسيم السورة إلى أجزاء إذا كانت أطول من الحد المسموح به
-            const messages = [];
-            for (let i = 0; i < ayahsText.length; i += maxMessageLength) {
-                messages.push(ayahsText.substring(i, i + maxMessageLength));
-            }
-
-            for (const message of messages) {
-                await api.sendMessage(message, threadID, messageID);
-            }
-        } catch (error) {
-            console.error(error);
-            return api.sendMessage("حدث خطأ أثناء جلب بيانات السورة. يرجى المحاولة مرة أخرى.", threadID, messageID);
+        if (!prefixN) {
+            return api.sendMessage("⚠️ | رد علي هذة الرسالة بالبادئة الجديدة", threadID, (err, info) => {
+              if (err) log.error(err)
+              if (event.type = "message_reaction" && event.messageReply.messageID === info.messageID) {
+                group.prefix = event.body
+                api.sendMessage(`⚠️ |تم تغيير البادئة الي ${event.body}` , threadID)
+              }
+            })
+    
         }
     }
 };
